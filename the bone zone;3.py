12243c00e823a93bf130c9345de1ea87c279ca96
GGtoEZ <50034506+GGtoEZ@users.noreply.github.com>
2019-05-07 17:35:09 -0700

    Add files via upload

---
+++ the bone zone;3.py
@@ -0,0 +1,57 @@
+import pygame, sys, random
+pygame.init()
+
+funzone = pygame.display.set_mode((500, 500))
+
+### Crab
+width = 50
+height = 50
+pX =250
+pY = 250
+
+vel = 10
+
+Swidth = 20
+Sheight = 20
+pygame.display
+
+### food!!!! ####
+
+foodX = random.randrange(500)
+foodY = random.randrange(500)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    keys=pygame.key.get_pressed()
+    funzone.fill((0,0,0))
+    myblock = pygame.draw.rect(funzone, (255,200,175), (foodX, foodY, 20,20))
+    pygame.draw.rect(funzone, (255, 0, 0), (pX, pY, width, height))
+    pygame.draw.rect(funzone, (255,255,255), (pX, pY, Swidth, Sheight))
+    pygame.draw.rect(funzone, (255,255,255), (pX+25,pY, Swidth, Sheight))
+
+    ### collision ###
+    
+
+    if any(pX <= X <= (pX + width) for X in range (foodX, foodX + Swidth)) and any(pY <= y <= (pX + height) for y in range (foodY, foodY + Swidth)) and any(pY <= y <= (pY + height) for y in range (foodY, foodY + Sheight)):
+        pygame.quit()
+        sys.exit()
+    
+    ### moving keys
+
+    if keys[pygame.K_a]:
+        if pX > 0:
+            pX-=vel
+    elif keys[pygame.K_d]:
+        if pX < 450:
+            pX+=vel
+    if keys[pygame.K_w]:
+        if pY > 0:
+            pY-=vel
+    elif keys[pygame.K_s]:
+        if pY < 450:
+            pY+=vel
+    
+    pygame.display.update()
